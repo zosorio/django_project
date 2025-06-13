fc728ddb8dfa847000952a6fdc1f68771a0a7757
= <=>
2025-06-13 10:47:13 -0500

    Feat: Modelos y migraciones con Django.
    Refs: https://github.com/zosorio/django_project/issues/1
    
    Signed-off-by: = <=>

--- django_project/django_app/views.py
+++ django_project/django_app/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def my_view(request):
-    return render(request=request, template_name="django_app/car_list.html")
+    car_list = [
+        {"title": "BMW"},
+        {"title": "Mazda"},
+    ]
+    context = {
+        "car_list": car_list
+    }
+    return render(request, "django_app/car_list.html", context)
